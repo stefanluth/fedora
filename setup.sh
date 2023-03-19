b65501eddae282a38d8c7082ee336667a5adcc89
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-19 22:10:41 +0100

    add docker suite

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ sudo dnf install \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
     -y
 
+# Add Docker Repo
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
 # Add VS Code Repo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -19,7 +24,12 @@ sudo dnf upgrade -y
 sudo dnf install \
     cargo \
     code \
+    containerd.io \
     docker \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
     dotnet \
     firefox \
     gh \
