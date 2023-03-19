fc762bec2baa95cc0bc41db9e89a86f575e54250
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-19 22:06:56 +0100

    add firefox settings

--- setup.sh
+++ setup.sh
@@ -81,6 +81,45 @@ firefox ublock_origin-1.47.2.xpi
 firefox bitwarden_password_manager-2023.2.1.xpi
 firefox reddit_enhancement_suite-5.22.15.xpi
 
+# Add Firefox settings
+sudo rm -f /usr/lib64/firefox/browser/defaults/preferences/firefox-redhat-default-prefs.js
+cd ~/.mozilla/firefox/*.default-release
+echo "user_pref(\"trailhead.firstrun.didSeeAboutWelcome\", true);" >> prefs.js
+
+# Disable telemetry
+echo "user_pref(\"app.shield.optoutstudies.enabled\", false);" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.feeds.telemetry\", false);" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.telemetry\", false);" >> prefs.js
+echo "user_pref(\"browser.ping-centre.telemetry\", false);" >> prefs.js
+echo "user_pref(\"datareporting.healthreport.uploadEnabled\", false);" >> prefs.js
+# Disable sponsored content
+echo "user_pref(\"browser.newtabpage.activity-stream.feeds.section.topstories\", false);" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned\", \"amazon\");" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines\", \"\");" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.section.highlights.includePocket\", false);" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.showSponsored\", false);" >> prefs.js
+echo "user_pref(\"browser.newtabpage.activity-stream.showSponsoredTopSites\", false);" >> prefs.js
+echo "user_pref(\"browser.search.hiddenOneOffs\", \"Google,Amazon.de,Bing,DuckDuckGo,Wikipedia (en)\");" >> prefs.js
+echo "user_pref(\"browser.topsites.blockedSponsors\", \"[\\\"amazon\\\",\\\"adidas\\\",\\\"nike\\\",\\\"trivago\\\"]\");" >> prefs.js
+echo "user_pref(\"services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored\", false);" >> prefs.js
+echo "user_pref(\"services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSites\", false);" >> prefs.js
+# Set homepage and pinned shortcuts
+echo "user_pref(\"browser.newtabpage.pinned\", \"[{\\\"url\\\":\\\"https://github.com\\\",\\\"label\\\":\\\"GitHub\\\"},{\\\"url\\\":\\\"https://youtube.com\\\",\\\"label\\\":\\\"YouTube\\\"},{\\\"url\\\":\\\"https://reddit.com\\\",\\\"label\\\":\\\"Reddit\\\"},{\\\"url\\\":\\\"https://outlook.live.com/owa/\\\",\\\"label\\\":\\\"Outlook\\\"}]\");" >> prefs.js
+echo "user_pref(\"browser.startup.homepage\", \"about:home\");" >> prefs.js
+echo "user_pref(\"browser.startup.page\", 1);" >> prefs.js
+echo "user_pref(\"browser.toolbars.bookmarks.visibility\", \"never\");" >> prefs.js
+# Disable autofill
+echo "user_pref(\"extensions.formautofill.creditCards.enabled\", false);" >> prefs.js
+echo "user_pref(\"signon.autofillForms\", false);" >> prefs.js
+echo "user_pref(\"signon.generation.enabled\", false);" >> prefs.js
+echo "user_pref(\"signon.management.page.breach-alerts.enabled\", false);" >> prefs.js
+# Move cache to RAM
+echo "user_pref(\"browser.cache.disk.enable\", false);" >> prefs.js
+echo "user_pref(\"browser.cache.disk.parent_directory\", \"/run/user/$(id -u)/firefox\");" >> prefs.js
+echo "user_pref(\"browser.cache.memory.enable\", true);" >> prefs.js
+# Enable DRM content
+echo "user_pref(\"media.gmp-widevinecdm.abi\", \"x86_64-gcc3\");" >> prefs.js
+
 # Change shells
 chsh -s $(which zsh)
 sudo chsh -s $(which zsh)
