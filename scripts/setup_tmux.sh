3bde01c68e1594258cfbb12b2fa33790c72c4e2c
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-03-29 00:53:08 +0200

    add tmux

---
+++ scripts/setup_tmux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! command -v tmux &> /dev/null
+then
+    echo "tmux could not be found"
+    exit
+fi
+
+source $HOME/.bashrc
+
+if [ -z "$DOTFILES_REPO_ROOT" ]; then
+    echo "DOTFILES_REPO_ROOT is not set. Please set it in your .bashrc."
+    exit 1
+fi
+
+echo "Copying tmux.conf..."
+curl -s "$DOTFILES_REPO_ROOT/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
