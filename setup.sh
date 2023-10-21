ae2d5ce42fed049c276c86e18eb2e5d0103e3292
stefanluth <stefan.luthardt@outlook.com>
2023-10-22 00:33:18 +0200

    update directory

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/stefanluth/dotfiles
 cd dotfiles/.config
 
 cp -r alacritty $CONFIG_DIR
-cp Code/User/settings.json $CONFIG_DIR/Code/User/settings.json
+cp vscode/settings.json $CONFIG_DIR/Code/User/settings.json
 cp .zshrc $HOME/.zshrc
 cp .tmux.conf $HOME/.tmux.conf
 cp .mozilla/user.js $HOME/.mozilla/firefox/*.default-release
