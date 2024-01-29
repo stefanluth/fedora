28676db70e344650685c0112bff0c71462b085e6
stefanluth <stefan.luthardt@outlook.com>
2024-01-29 01:38:15 +0100

    make required dirs

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 CONFIG_DIR=$HOME/.config
 
-mkdir -p $CONFIG_DIR
+mkdir -p $CONFIG_DIR/Code/User/
 cd $CONFIG_DIR
 git clone https://github.com/stefanluth/dotfiles
 cd dotfiles/.config
 
 cp -r alacritty $CONFIG_DIR
+cp .prettierrc.json $CONFIG_DIR
 cp vscode/settings.json $CONFIG_DIR/Code/User/settings.json
 cp .zshrc $HOME/.zshrc
 cp .tmux.conf $HOME/.tmux.conf
