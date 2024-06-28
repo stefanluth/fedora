d225dd77650fd67d32cae3fa2104cc5c1d275fa8
stefanluth <stefan.luthardt@outlook.com>
2024-06-28 16:41:54 +0200

    Install oh-my-zsh

--- install/packages.sh
+++ install/packages.sh
@@ -54,9 +54,10 @@ sudo dnf install \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
     -y
 
-echo "Installing packages..."
+echo "Updating packages..."
 sudo dnf update -y
 sudo dnf upgrade -y
+echo "Installing packages..."
 sudo dnf install ${PACKAGES[@]} --allowerasing --skip-broken -y
 
 echo "Adding user to pkg-build group..."
@@ -64,6 +65,9 @@ sudo usermod -a -G pkg-build $USER
 
 echo "Installing rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Getting tpm..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
