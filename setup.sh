9cdac7b965760b7d1a17cc62c0689dd8184a8229
stefanluth <stefan.luthardt@outlook.com>
2024-06-28 16:50:33 +0200

    Use absolute paths

--- setup.sh
+++ setup.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-cd $HOME
-git clone https://github.com/stefanluth/dotfiles
-cd dotfiles
+git clone https://github.com/stefanluth/dotfiles $HOME/dotfiles
 
-bash install/packages.sh
-bash install/alacritty.sh
-bash install/vscode.sh
-bash install/fonts.sh
+bash $HOME/dotfiles/install/packages.sh
+zsh
+bash $HOME/dotfiles/install/alacritty.sh
+bash $HOME/dotfiles/install/vscode.sh
+bash $HOME/dotfiles/install/fonts.sh
 
-bash install/extensions/firefox.sh
-bash install/extensions/vscode.sh
-bash install/extensions/gnome.sh
-bash install/extensions/zsh.sh
+bash $HOME/dotfiles/install/extensions/firefox.sh
+bash $HOME/dotfiles/install/extensions/vscode.sh
+bash $HOME/dotfiles/install/extensions/gnome.sh
+bash $HOME/dotfiles/install/extensions/zsh.sh
 
-bash scripts/git.sh
+bash $HOME/dotfiles/scripts/git.sh
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 mkdir -p $HOME/.config/Code/User/
 
-cp -a dotfiles/. $HOME
-mv dotfiles/.mozilla/firefox/user.js $HOME/.mozilla/firefox/*.default-release
+cp -a $HOME/dotfiles/dotfiles/. $HOME
+mv $HOME/dotfiles/dotfiles/.mozilla/firefox/user.js $HOME/.mozilla/firefox/*.default-release
