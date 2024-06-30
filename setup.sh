a5253c7b13461709bf2f47d5d59e82c3317d7a82
stefanluth <stefan.luthardt@outlook.com>
2024-07-01 01:04:47 +0200

    Add docker install script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,18 @@
 
 git clone https://github.com/stefanluth/dotfiles $HOME/dotfiles
 
+echo "Installing packages..."
 bash $HOME/dotfiles/install/packages.sh
+
 zsh
+
+echo "Installing alacritty..."
 bash $HOME/dotfiles/install/alacritty.sh
+echo "Installing docker..."
+bash $HOME/dotfiles/install/docker.sh
+echo "Installing vscode..."
 bash $HOME/dotfiles/install/vscode.sh
+echo "Installing fonts..."
 bash $HOME/dotfiles/install/fonts.sh
 
 bash $HOME/dotfiles/install/extensions/firefox.sh
