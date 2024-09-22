6a963edac224ba7c4e93fb8591114adec09945b1
stefanluth <stefan.luthardt@outlook.com>
2024-09-22 17:28:19 +0200

    Add gnome setup script

---
+++ scripts/gnome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.input-sources show-all-sources true
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize:'
