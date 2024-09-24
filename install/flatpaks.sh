8c3b3788bd118a5e6764c392d552d89fec5de6ff
stefanluth <stefan.luthardt@outlook.com>
2024-09-24 10:07:19 +0200

    Add flatpaks

---
+++ install/flatpaks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub com.github.Matoking.protontricks
+flatpak install flathub org.telegram.desktop
--- setup.sh
+++ setup.sh
@@ -2,21 +2,46 @@
 
 git clone https://github.com/stefanluth/dotfiles $HOME/dotfiles
 
+echo "---------------------------------------------------"
 echo "Installing packages..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/packages.sh
-
+echo "---------------------------------------------------"
+echo "Installing flatpaks..."
+echo "---------------------------------------------------"
+bash $HOME/dotfiles/install/flatpaks.sh
+echo "---------------------------------------------------"
 echo "Installing alacritty..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/alacritty.sh
+echo "---------------------------------------------------"
 echo "Installing docker..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/docker.sh
+echo "---------------------------------------------------"
 echo "Installing vscode..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/vscode.sh
+echo "---------------------------------------------------"
 echo "Installing fonts..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/fonts.sh
 
+echo "---------------------------------------------------"
+echo "Installing Firefox extensions..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/extensions/firefox.sh
+echo "---------------------------------------------------"
+echo "Installing VSCode extensions..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/extensions/vscode.sh
+echo "---------------------------------------------------"
+echo "Installing Gnome extensions..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/extensions/gnome.sh
+echo "---------------------------------------------------"
+echo "Installing zsh extensions..."
+echo "---------------------------------------------------"
 bash $HOME/dotfiles/install/extensions/zsh.sh
 
 bash $HOME/dotfiles/scripts/gnome.sh
