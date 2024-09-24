a78eb312824eb6075eafe262ed7b65600de139b4
stefanluth <stefan.luthardt@outlook.com>
2024-09-24 10:07:11 +0200

    Add JetBrains Mono Nerd Font

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+mkdir -p ~/.local/share/fonts/JetBrainsMono
+cd ~/.local/share/fonts/JetBrainsMono
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+tar -xf JetBrainsMono.tar.xz
+rm JetBrainsMono.tar.xz
+sudo chown -R root: /usr/local/share/fonts/JetBrainsMono
+sudo chmod 644 /usr/local/share/fonts/JetBrainsMono/*
+sudo restorecon -vFr /usr/local/share/fonts/JetBrainsMono
+sudo fc-cache -f -v
